7e3c2af429d0c1a5d9337fbcafa50e16b8148ac0
Kimberly D Hendrick <kimdbarnes@gmail.com>
2016-12-29 11:02:53 -0700

    Finished video #12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,20 +3,49 @@ import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
 	// basic component
+
+	// need ctor to bind "this"
+	// constructor() {
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+
+	goToStore(event) {
+		event.preventDefault();
+
+		const storeId = this.storeInput.value;
+
+		// first grab text from box
+		console.log(storeId);
+
+		// second transition from / to /store/:storeId
+		this.context.router.transitionTo(`store/${storeId}`);
+	}
+
 	// needs at least the render() method
 	render() {
 		return (
 			// only one parent element in the render
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => {this.goToStore(e)}}>
+			{ /*  another way to make sure this is bound: */ }
+			{ /* <form className="store-selector" onSubmit={this.goToStore.bind(this)}> */ }
 			{ /* comments inside JSX */ }
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name"
-				defaultValue={getFunName()}/>
+				<input type="text" 
+					required 
+					placeholder="Store Name"
+					defaultValue={getFunName()} 
+					ref={(input) => { this.storeInput = input; } }
+				/>
 				<button type="submit">Visit Store -></button>
 				
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
