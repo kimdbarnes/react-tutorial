10ff0b10a6a331faa527364bb1ebf1266d6e93e7
Kimberly D Hendrick <kimdbarnes@gmail.com>
2017-01-08 11:17:34 -0700

    Finishes video #18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDZ97IlvDW1PNlNTu_-zp4RWRWVUh5lozE",
+    authDomain: "catch-of-the-day-kimb.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-kimb.firebaseio.com",
+});
+
+export default base;
